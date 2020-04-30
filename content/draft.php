dfa41823f65632bb7eaf77be822252c25465b4b7
bsaxen <benny.saxen@gmail.com>
2020-04-30 16:58:38 +0200

    Create draft.php

---
+++ content/draft.php
@@ -0,0 +1,12 @@
+Allt handlar ytterst om information. Det finns ingen verksamhet som inte är beroenda av hur effektivt man hanterar information.
+Hanteringen av information innebär att:
+
+- samla in information där den skapas
+- integrera med redan skapad information
+- representera information utan extra bagage
+- använda information där den behövs, t ex applikationer, människor, m m 
+- 360 graders vy av informationen, d v s dra nya slutsatser.
+
+
+Det är viktigt att göra åtskillnad på data och information. Data är olika representationer av information.T ex kan information representeras binärt av 1-or och 0-or.
+De utmaningar som är förknippat med hantering av data handlar oftast och lagring och transport. 
