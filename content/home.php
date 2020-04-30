a8a75e192cfb168070447bc2d5b61bd121d9e86b
bsaxen <benny.saxen@gmail.com>
2020-04-30 16:53:58 +0200

    Update home.php

--- content/home.php
+++ content/home.php
@@ -1,3 +1,10 @@
 <?php
 echo "<h3>Information</h3>";
 ?>
+<ul>
+<li>Frikoppla information ifrån applikation och infrastruktur</li>
+<li>Representera information baserat på informationens grundstenar</li>
+<li>Data och information är inte samma sak</li>
+</ul>
+
+
