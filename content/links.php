5f96a844b7aa69e770fcca29ac6f4fc52a1ab26d
bsaxen <benny.saxen@gmail.com>
2020-05-05 13:23:19 +0200

    Update links.php

--- content/links.php
+++ content/links.php
@@ -1,3 +1,7 @@
 <?php
 echo "<h3>Länkar</h3>";
 ?>
+<ul>
+  <li><a href="https://www.w3.org/RDF">RDF Standard</a></li>
+  <li><a href="https://blog.liu.se/olafhartig/">Olaf Hartig Blog Linköping University</a></li>
+</ul>
